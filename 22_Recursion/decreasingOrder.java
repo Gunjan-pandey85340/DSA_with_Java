352bb01e180a37d2c50880fc93b8f9f30e06e75f
Gunjan-pandey85340 <gunjan.pandey_cs22@gla.ac.in>
2025-03-20 09:20:01 +0530

    recursion

---
+++ 22_Recursion/decreasingOrder.java
@@ -0,0 +1,15 @@
+public class decreasingOrder {
+    public static void decOrder(int n){
+        if(n == 1) {
+            System.out.println(n);
+            return ;
+        }
+        
+        System.out.print(n+" ");
+        decOrder(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        decOrder(n);;
+    }
+}
